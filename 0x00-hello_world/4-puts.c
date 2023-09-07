7d0966dd65b16706867ca3f9024bd8f849d0a75f
reela <sudanrela123@gmail.com>
2023-09-07 10:46:04 -0700

    my

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
-int main () {
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);}
+/**
+ * main -Entry point
+ * Description: C program to print
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
