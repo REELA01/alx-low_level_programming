62f326b2bb57b3755f33464c299ac8eaece7e618
reela <sudanrela123@gmail.com>
2023-09-08 12:37:48 -0700

    my

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: writing alpha
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= z)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
