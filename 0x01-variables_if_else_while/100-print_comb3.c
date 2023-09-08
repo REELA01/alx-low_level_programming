aff9a43e0e5a3898b024a93bbe949f13e4bda1f3
reela <sudanrela123@gmail.com>
2023-09-08 16:23:49 -0700

    my

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,11 +6,11 @@
 */
 int main(void)
 {
-	int n;
+	int n, m;
 
 	for (n = 0 ; n <= 9 ; n++)
 	{
-		for (int m = 0 ; m <= 9 ; m++)
+		for (m = 0 ; m <= 9 ; m++)
 		{
 			putchar(n + '0');
 			putchar(m + '0');
