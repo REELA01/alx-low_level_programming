eb8fe60b12c7133eebead24baee6cc30e386fdf3
reela <sudanrela123@gmail.com>
2023-09-08 14:27:02 -0700

    my

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descripition: hex
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int n = 0;
+	char c = 'a';
+
+	while (n <= 9)
+	{
+		putchar(n);
+		n++;
+	}
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
