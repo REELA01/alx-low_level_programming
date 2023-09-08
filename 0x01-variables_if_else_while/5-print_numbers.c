7ea4fdbe1eea0331debc90402c29792886707ee7
reela <sudanrela123@gmail.com>
2023-09-08 13:35:43 -0700

    my

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: dec
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
