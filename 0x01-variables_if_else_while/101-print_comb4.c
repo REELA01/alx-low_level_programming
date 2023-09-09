f76bbe4c897710ec65ac30d669f61da599198749
reela <sudanrela123@gmail.com>
2023-09-08 17:38:49 -0700

    my

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,8 +17,11 @@ int main(void)
 				putchar(n + '0');
 				putchar(m + '0');
 				putchar(r + '0');
-				putchar(',');
-				putchar(' ');
+				if ( m + n + r != 24)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
