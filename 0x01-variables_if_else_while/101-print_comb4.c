245ea713fdb0664add2d4afa3ebdcc9fcf02686a
reela <sudanrela123@gmail.com>
2023-09-08 17:24:54 -0700

    my

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: combnation
+ * Return: 0 success
+*/
+int main(void)
+{
+	int n, m, r;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		for (m = n + 1 ; n <= 9 ; m++)
+		{
+			for (r = m + 1 ; r <= 9 ; r++)
+			{
+				putchar(n + '0');
+				putchar(m + '0');
+				putcahr(r + '0');
+				if (m + n + r  == 24)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
