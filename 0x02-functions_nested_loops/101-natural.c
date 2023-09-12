499a867121539117ea56a18c7996e909c2731d3c
reela <sudanrela123@gmail.com>
2023-09-12 16:27:57 -0700

    my

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+/**
+ * main - Entry point
+ * description: all mutliplr tp 1024
+ * Return: 0 success
+*/
+int main(void)
+{
+	int n, count;
+
+	for (n = 0 ; n <= 1024 ; n++)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+		count = count + n;
+	}
+	return (count);
+}
