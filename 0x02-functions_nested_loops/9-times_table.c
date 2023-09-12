52805195ffe65fe84139929bc5c53a9416663f4b
reela <sudanrela123@gmail.com>
2023-09-12 13:39:44 -0700

    my

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			_putchar((n * m) + '0');
+			_putchar((i * j) + '0');
 			_putchar(',');
 			if (i != 9 || j != 9)
 			_putchar(' ');
