258ff0c16313fde76b13c75f6093b530f3b98c3a
reela <sudanrela123@gmail.com>
2023-09-12 16:52:33 -0700

    my

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,14 +6,15 @@
 */
 int main(void)
 {
-	int m, n1 = 0, n2 = 1, n3;
+	int m;
+	unsigned long n1 = 0, n2 = 1, sum;
 
 	for (m = 0 ; m < 50 ; m++)
 	{
-		n3 = n1 + n2;
-		printf("%d", n3);
+		sum = n1 + n2;
+		printf("%lu", sum);
 		n1 = n2;
-		n2 = n3;
+		n2 = sum;
 		if (m == 49)
 		printf("\n");
 		else
