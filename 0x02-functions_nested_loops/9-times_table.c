2b057dc2eb89c691ebe372353e0bf4f79a64972e
reela <sudanrela123@gmail.com>
2023-09-12 13:38:24 -0700

    my

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - do 9x9 table
+*/
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 9 ; j++)
+		{
+			_putchar((n * m) + '0');
+			_putchar(',');
+			if (i != 9 || j != 9)
+			_putchar(' ');
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
