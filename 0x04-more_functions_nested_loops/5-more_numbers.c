90445d2367a133d4d255e6c2d21226efb807a782
reela <sudanrela123@gmail.com>
2023-09-14 11:05:00 -0700

    my

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - to print to 14
+*/
+void more_numbers(void)
+{
+	int n, m, count;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (count = 0; count <= 14; count++)
+		{
+			m = count;
+			if (count > 9)
+			{
+				_putchar(1 + '0');
+				m = count % 10;
+			}
+			_putchar(m + '0');
+		}
+		_putchar('\n');
+	}
+}
