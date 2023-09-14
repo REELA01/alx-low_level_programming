48d89d8af918175aa83c1559419b45b2838e786b
reela <sudanrela123@gmail.com>
2023-09-14 13:00:46 -0700

    my

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: buzz fizz
+ * Return: multiples of three print Fizz instead of the number
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+		putchar('\n');
+	}
+	return (0);
+}
