63451409fda8a1509bb3d1a35c134cb7e0d09062
reela <sudanrela123@gmail.com>
2023-09-18 11:57:26 -0700

    my

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - takes a pointer to an int
+ * @a: parameter
+ * @n: array elements
+ * Return: nothing
+*/
+void print_array(int *a, int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		if (t != n - 1)
+			printf("%d, ", a[t]);
+		else
+			printf("%d", a[t]);
+	}
+	printf("\n");
+}
