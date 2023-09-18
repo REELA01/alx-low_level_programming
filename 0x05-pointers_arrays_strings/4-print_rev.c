55493a95fd3b8933f47f8c8b9d5c4c9ee7390064
reela <sudanrela123@gmail.com>
2023-09-18 11:00:15 -0700

    my

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - takes a pointer to char
+ * @s: parameter
+ * Return: nothing
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+	while(s[i])
+		i++;
+	while(i)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
