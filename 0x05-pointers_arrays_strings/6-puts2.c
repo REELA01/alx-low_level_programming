2079467f7375711ee5bd4eeb6bc84c77e419164e
reela <sudanrela123@gmail.com>
2023-09-18 11:29:18 -0700

    my

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - takes a pointer ant print the other char
+ * @str: parameter
+ * Return: nothing
+*/
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
