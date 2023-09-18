07a205dd82ab3df9bc6907259d1bf708f332e693
reela <sudanrela123@gmail.com>
2023-09-18 11:37:54 -0700

    my

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts_half - split the string
+ * @str: parameter
+ * Return: nothing
+*/
+void puts_half(char *str)
+{
+	int l;
+
+	while (str[l] != '\0')
+		l++;
+	for (l /= 2; str[l] != '\0'; l++)
+		_putchar(str[l]);
+	_putchar('\n');
+}
