75c5525840afd9bf4d28562bfd485fd63d99e076
reela <sudanrela123@gmail.com>
2023-09-18 11:20:36 -0700

    my

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - takes a pointer to reverse
+ * @s: parameter
+ * Return: nothing
+*/
+void rev_string(char *s)
+{
+	int l = 0, i;
+	char tmp;
+
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	for (i = 0; i < l / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = tmp;
+	}
+}
