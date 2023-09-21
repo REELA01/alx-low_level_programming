cbbc58fc281dbf3addb468dd036dd5771d50eda2
reela <sudanrela123@gmail.com>
2023-09-21 02:23:14 -0700

    my

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * *rot13 - rot13 role
+ * @s: parameter
+ * Return: s value
+*/
+char *rot13(char *s)
+{
+	int i, ro;
+
+	char m[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+	char j[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (ro = 0; ro < 52; ro++)
+		{
+			if (m[ro] == s[i])
+			{
+				s[i] == j[ro];
+				break;
+			}
+		}
+	}
+	return (s);
+}
