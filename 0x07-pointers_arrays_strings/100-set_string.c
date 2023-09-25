06fd8af73d8c22a57947866712fcd57a1b86a068
reela <sudanrela123@gmail.com>
2023-09-25 14:33:45 -0700

    my

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - pointer pointing to piointer pointing to character
+ * @to: parameter
+ * @s: pointer pointer
+ * Return: dest value
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
