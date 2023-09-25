1850fc3b522676f3e7fb1eeeb76f6c44fbdec4b4
reela <sudanrela123@gmail.com>
2023-09-25 11:51:32 -0700

    my

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagsums - sum of diagsums
+ * @a: parameter
+ * @size: parameter
+ * Return: none value
+*/
+void print_diagsums(int *a, int size)
+{
+	int i, c1 = 0, c2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		c1 += a[i];
+		c2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, ", c1);
+	printf("%d\n", c2);
+}
