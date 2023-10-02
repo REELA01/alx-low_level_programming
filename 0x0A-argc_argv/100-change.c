5baf8e48c3b8c86a211cb842ee8b404aa46aba0b
reela <sudanrela123@gmail.com>
2023-10-02 14:49:21 -0700

    y

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: int
+ * @argv:char
+ * Return: 0 success
+*/
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, lc = 0, money = atoi(argv[1]);
+		int c[] = {25, 10, 5, 2, 1};
+
+		for (i = 0; i < 5; i++)
+		{
+			if (money >= c[i])
+			{
+				lc += money / c[i];
+				money = money % c[i];
+				if (money % c[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+		printf("%d\n", lc);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
