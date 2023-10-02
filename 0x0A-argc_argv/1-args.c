50973af2d2d3a1d2644768d19019df8376dd8eac
reela <sudanrela123@gmail.com>
2023-10-02 09:14:21 -0700

    y

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: int
+ * @argv:char
+ * Return: 0 success
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv[];
+	printf("%d\n", argc - 1);
+	return (0);
+}
