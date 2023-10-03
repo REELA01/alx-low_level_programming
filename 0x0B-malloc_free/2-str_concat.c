69f69e44416e5beeda7d545e8c62220ad568f2b5
reela <sudanrela123@gmail.com>
2023-10-03 11:41:11 -0700

    y

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,9 +24,9 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 	si1 = _strlen(s1);
 	si2 = _strlen(s2);
 	m = malloc((si1 + si2) * sizeof(*s1) + 1);
