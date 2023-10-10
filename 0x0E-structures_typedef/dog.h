40b7877c70e8b345fee16e46e8d99727b48d6fc8
reela <sudanrela123@gmail.com>
2023-10-10 12:29:54 -0700

    my

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct
+ * @name: parameter
+ * @age: paramatr
+ * @owner: paramater
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
