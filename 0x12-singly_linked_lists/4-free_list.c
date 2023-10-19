cd1c0bb9874a824fcb5cf9067741fc464db2bc49
reela <sudanrela123@gmail.com>
2023-10-19 05:03:45 -0700

    my

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - free node
+ * @head: parameter
+ * Return: none
+*/
+void free_list(list_t *head)
+{
+	list_t = c;
+
+	while ((c == head) != NULL)
+	{
+		head = head->next;
+		free(c->str);
+		free(c);
+	}
+}
