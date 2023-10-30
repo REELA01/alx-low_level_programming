999f4b2c6f0e7858389a287f4b54299021540adf
reela <sudanrela123@gmail.com>
2023-10-30 03:05:31 -0700

    my

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _strlen - the same function
- * @s: parameter
- * Return: length
+  * _strlen - find the length
+  * @s: parameter
+  * Return: length
 */
 int _strlen(char *s)
 {
@@ -13,19 +13,19 @@ int _strlen(char *s)
 	return (i);
 }
 /**
- * create_file - create file
- * @filename: parameter
- * @text_content: tne buffer
- * Return: 1 on success, -1 on failure
+* append_text_to_file - append text
+* @filename: the file
+* @text_content: the buffer
+* Return: 1 on success, -1 on failure
 */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t w;
 	int fd;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	if (text_content)
