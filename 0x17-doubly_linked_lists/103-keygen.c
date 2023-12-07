fcaef1c3b8a21530d1fb8a65e443b9f322f6a865
reela <sudanrela123@gmail.com>
2023-12-07 09:53:34 -0800

    last

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,43 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
- * main - Generates and prints passwords for crackme5
- * @argc: arguments
- * @argv: pointer argument array
- * Return: Always 0
-*/
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	char password[7], *codex;
-	int len = strlen(argv[1]), i, current;
+	int len = strlen(argv[1]), i, tmp;
 
 	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	current = (len ^ 59) & 63;
-	password[0] = codex[currnet];
-	current = 0;
+
+	tmp = (len ^ 59) & 63;
+	password[0] = codex[tmp];
+
+	tmp = 0;
 	for (i = 0; i < len; i++)
-		current += argv[1][i];
-	password[1] = codex[(current ^ 79) & 63];
-	currnet = 1;
+		tmp += argv[1][i];
+	password[1] = codex[(tmp ^ 79) & 63];
+
+	tmp = 1;
 	for (i = 0; i < len; i++)
-		current *= argv[1][i];
-	password[2] = codex[(current ^ 85) & 63];
-	current = 0;
+		tmp *= argv[1][i];
+	password[2] = codex[(tmp ^ 85) & 63];
+
+	tmp = 0;
 	for (i = 0; i < len; i++)
 	{
-		if (argv[1][i] > current)
-			current = argv[1][i];
+		if (argv[1][i] > tmp)
+			tmp = argv[1][i];
 	}
-	srand(current ^ 14);
+	srand(tmp ^ 14);
 	password[3] = codex[rand() & 63];
-	current = 0;
+
+	tmp = 0;
 	for (i = 0; i < len; i++)
-		current += (argv[1][i] * argv[1][i]);
-	password[4] = codex[(current ^ 239) & 63];
+		tmp += (argv[1][i] * argv[1][i]);
+	password[4] = codex[(tmp ^ 239) & 63];
+
 	for (i = 0; i < argv[1][0]; i++)
-		current = rand();
-	password[5] = codex[(current ^ 229) & 63];
+		tmp = rand();
+	password[5] = codex[(tmp ^ 229) & 63];
+
 	password[6] = '\0';
 	printf("%s", password);
 	return (0);
